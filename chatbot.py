5c239a49f2cd61cd2def30aedcf66a34443968fb
kenliong <6667431+kenliong@users.noreply.github.com>
2023-08-17 12:07:09 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 from utils import *
 import streamlit as st
-
+st.markdown(get_custom_css_modifier(), unsafe_allow_html=True)
 st.title("💬 PA facility booking concierge")
 
 if "messages" not in st.session_state:
@@ -13,8 +13,9 @@ if prompt := st.chat_input():
 
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
+    
+    response = respond_to_user_input(prompt, st.session_state.messages)
 
-    response = respond_to_user_input(prompt)
-
-    st.session_state.messages.append({"role": "assistant", "content": response.content})
-    st.chat_message("assistant").write(response.content)
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.chat_message("assistant").write(response)
+    st.experimental_rerun()
