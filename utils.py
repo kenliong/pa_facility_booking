20f49a647e9911481b294a7e0d93920168f555ff
kenliong <6667431+kenliong@users.noreply.github.com>
2023-08-18 16:31:11 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.output_parsers import PydanticOutputParser
 from langchain.chains import ConversationChain
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Union
 
 def get_custom_css_modifier():
     css_modifier = """
@@ -138,7 +138,7 @@ def get_calendar_reference():
 
 def get_interested_dates(date_str):
     class InterestedDates(BaseModel):
-        interested_dates: List[str] = Field(description='a list of dates in dd/mm/yyyy format that the client could be interested in when booking the facility. If this information is not found, output [-1].')
+        interested_dates: List[Union[str,int]] = Field(description='a list of dates in dd/mm/yyyy format that the client could be interested in when booking the facility. If this information is not found, output [-1].')
 
     parser = PydanticOutputParser(pydantic_object=InterestedDates)
     format_instructions = parser.get_format_instructions()
@@ -401,8 +401,8 @@ def parse_user_input(user_input, session_state_chat_history):
     
     class UserInput(BaseModel):
         type_of_request: str = Field(description='the type of request that the client is making based on the latest message. This will be one of the following values: <request_question>, <conversational_message>')
-        interested_locations: List[str] = Field(description='a list of all locations that the client might be interested in when booking the facility. If this information is not found, output ["-1","-1"]')
-        interested_dates: List[str] = Field(description='a list of all relative date expressions that the client might be interested in when booking the facility. If this information is not found, output ["-1","-1"]')
+        interested_locations: List[Union[str,int]] = Field(description='a list of all locations that the client might be interested in when booking the facility. If this information is not found, output ["-1"]')
+        interested_dates: List[Union[str,int]] = Field(description='a list of all relative date expressions that the client might be interested in when booking the facility. If this information is not found, output ["-1"]')
     
     parser = PydanticOutputParser(pydantic_object=UserInput)
     format_instructions = parser.get_format_instructions()
@@ -422,8 +422,8 @@ Latest human input: ```{user_input}```
 Extract the following information:
 
 type_of_request: The type of request that the client is making making based on the latest message. This will be one of the following values: <request_question>, <conversational_message>
-location: A list of all potential locations that the client is interested in. If this information is not found, output ["-1","-1"].
-requested_date: A list of all relative date expressions that the client is interested in. Copy the exact text as provided by the client without making any additional inferences. If this information is not found, output ["-1","-1"].
+location: A list of all potential locations that the client is interested in. If this information is not found, output ["-1"].
+requested_date: A list of all relative date expressions that the client is interested in. Copy the exact text as provided by the client without making any additional inferences. If this information is not found, output ["-1"].
 
 {format_instructions}
     """
